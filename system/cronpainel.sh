20fbca31dc2914bac145bbb732c5d19e34c44f83
system-net <103401482+system-net@users.noreply.github.com>
2022-05-18 21:53:31 -0400

    Add files via upload

---
+++ system/cronpainel.sh
@@ -0,0 +1,8 @@
+while true 
+do 
+  /usr/bin/php /var/www/html/pages/system/cron.php
+  /usr/bin/php /var/www/html/pages/system/cron.ssh.php
+  /usr/bin/php /var/www/html/pages/system/cron.online.ssh.php
+  echo "10 segundos" 
+sleep 10
+done
