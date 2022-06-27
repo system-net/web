f0360d5e3ad30582ed757e69d870f8a562e29f79
system-net <103401482+system-net@users.noreply.github.com>
2022-06-27 19:18:27 -0400

    Add files via upload

--- system/restaurar.sh
+++ system/restaurar.sh
@@ -4,14 +4,6 @@
 #----------------------------------------------------------------------
 clear
 crz=$(echo -e "\033[1;31m           @Universe_say\033[1;32m")
-echo """              $crz             
- _____ _____ _____ _____ _         
-|   __|   __|  |  |  _  | |_ _ ___ 
-|__   |__   |     |   __| | | |_ -|
-|_____|_____|__|__|__|  |_|___|___|
-                      """
-echo -e "\033[1;33mESSE SCRIPT RESTAURA O BANCO DE DADOS\n(BACKUP) DO PAINEL !\033[0m" 
-echo -e "\n\033[1;33mÉ NECESSÁRIO O PAINEL INSTALADO E O\nAQUIVO (\033[1;32msshplus.sql\033[1;33m) NO DIRETORIO ROOT !\033[0m\n" 
 echo -ne "\033[1;32mEnter para continuar...\033[0m"; read
 
 [[ ! -e /var/www/html/pages/system/pass.php ]] && {
